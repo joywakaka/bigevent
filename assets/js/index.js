99ad21bffc3ce34d5968e70a990957b066babdb6
yuli <624996569@qq.com>
2020-11-25 15:52:54 +0800

    master index and login 更新

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,22 @@
-// 在dom树加载完成后开始执行
 $(function() {
-    getUserInfo();
-    $("#btnLogout").on('click', logout);
+
 })
+
+// 获取用户数据 并渲染到页面的对应位置
 
 function getUserInfo() {
     $.ajax({
-        method: 'get',
         url: '/my/userinfo',
-        headers: {
+        method: 'get',
+        header: {
             Authorization: localStorage.getItem('token')
         },
         success(res) {
             // console.log(res);
-            if (res.status !== 0) {
-                return layui.layer.msg('获取用户信息失败！')
+            if (res.tatus !== 0) {
+                return layui.layer.msg('获取用户信息失败!')
             }
+            //调用修改用户信息的函数
             renderAvatar(res.data)
         }
     })
@@ -24,25 +25,24 @@ function getUserInfo() {
 function renderAvatar(user) {
     var name = user.nickname || user.username;
     $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
-    //按需渲染用户头像
+    //按需渲染用户头像'
     if (user.user_pic !== null) {
         $('.layui-nav-img').attr('src', user.user_pic).show();
         $('.text-avatar').hide();
     } else {
         $('.layui-nav-img').hide();
-        //选择第一个字母 并大写
+        //选择用户名的第一个字母 并大写填充在图片的位置
         var first = name[0].toUpperCase();
         $('.text-avatar').text(first).show();
     }
 }
 
 function logout() {
-    layui.layer.confirm('确定退出吗？', { icon: 3, title: '提示' }, function(index) {
-        //删除localStorage中的token 值
+    lay.layer.confirm('确定退出吗?', { icon: 3, title: '提示' }, function(index) {
+        //删除localStorge 的token值
         localStorage.removeItem('token');
-        // 跳转到login.html
+        //跳转到login.html
         location.href = '/login.html';
         layer.close(index);
-
     })
 }
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,81 +1,103 @@
+// 给去登陆和去注册添加点击事件 实现互相跳转
 $(function() {
-    //点击去注册链接
     $('#link_reg').on('click', function() {
-        $('.login-box').hide()
-        $('.reg-box').show()
+        $('.login-box').hide();
+        $('.reg-box').show();
+    })
+    $('#link_login').on('click', function() {
+        $('.login-box').show();
+        $('.reg-box').hide();
     })
 })
 
-$(function() {
-        //点击去登录
-        $('#link_login').on('click', function() {
-            $('.login-box').show()
-            $('.reg-box').hide()
-        })
-    })
-    //从layui获取form对象
+// 注册页面表单验证
 
 layui.form.verify({
-        //自定义pwd 规则
-        pwd: [
-            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
-        ],
-        ////通过形参拿到的是确认密码的值
-        repwd: function(pwd2) {
-            var pwd1 = $('.reg-box [name=password').val();
-            //判断两次密码是否一致
-            if (pwd1 != pwd2) return '两次输入的密码不一致哈'
+    // username: function(value, item) { //value：表单的值、item：表单的DOM对象
+    //     if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
+    //         return '用户名不能有特殊字符';
+    //     }
+    //     if (/(^\_)|(\__)|(\_+$)/.test(value)) {
+    //         return '用户名首尾不能出现下划线\'_\'';
+    //     }
+    //     if (/^\d+\d+\d$/.test(value)) {
+    //         return '用户名不能全为数字';
+    //     }
+    // },
+
+    //我们既支持上述函数式的方式，也支持下述数组的形式
+    //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+    pwd: [
+        /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+    ],
+
+    repwd: function(pwd2) {
+        var pwd1 = $('.reg-box [name=password]').val();
+        if (pwd2 !== pwd1) {
+            return '两次输入的密码不一致'
         }
+    }
+});
 
-    })
-    // 登录跳转部分
-    // let baseUrl = "http://ajax.frontend.itheima.net";
-
-
-
+//给表单添加提交事件 并调用一个函数
 $('#regForm').on('submit', submitData);
-$('#form_login').on('submit', function(e) {
-        e.preventDefault();
-        let dataStr = $(this).serialize();
-        $.ajax({
-            url: '/api/login',
-            method: 'post',
-            data: dataStr,
-            success(res) {
-                if (res.status != 0) return layui.layer.msg(res.message);
-                layui.layer.msg(res.message, {
-                    icon: 6,
-                    time: 1500,
 
-                }, function() {
-                    // 将登录成功的token保存到本地存储
-                    localStorage.setItem('token', res.token);
-                    location.href = '/index.html'
-                })
-            }
-        })
-    })
-    //监听注册表单提交事件  提交之后就会传输数据字符串
 function submitData(e) {
     e.preventDefault();
     let datastr = $(this).serialize();
+    // console.log(datastr);
+    // console.log(this);
     $.ajax({
         url: '/api/reguser',
         method: 'post',
         data: datastr,
         success(res) {
+            console.log(res);
             if (res.status != 0) return layui.layer.msg(res.message);
-            // console.log(res.message);
             layui.layer.msg(res.message);
+            // 注册成功后直接把表单数据加到登录页面
             let uname = $('.reg-box [name=username]').val().trim();
-            $('.login-box [name=username').val(uname);
+            $('.login-box [name=username]').val(uname);
             let upwd = $('.reg-box [name=password]').val().trim();
-            $('.login-box [name=password').val(upwd);
-            // 清空注册表单
+            $('.login-box [name=password]').val(upwd);
+            //清空注册页面的表单内容
             $('#regForm')[0].reset();
-            // 切换到登录idv
+            //切换到登录页面 相当于模拟点击去登录连接的事件
             $('#link_login').click();
+
+        }
+    })
+}
+
+//登录页页面操作 
+$('#form_login').on('submit', function(e) {
+    e.preventDefault();
+    let dataStr = $(this).serialize();
+    $.ajax({
+        url: '/api/login',
+        method: 'post',
+        data: dataStr,
+        success(res) {
+            // console.log(res);
+            if (res.status != 0) return layui.layer.msg(res.message);
+            //用了layui的方法 弹出层延时 和类型
+            // layui.layer.layer.msg('同上', {
+            //     icon: 1,
+            //     time: 2000 //2秒关闭（如果不配置，默认是3秒）
+            // }, function() {
+            //     //do something
+            // });
+            layui.layer.msg(res.message, {
+                icon: 6,
+                time: 1500,
+            }, function() {
+                //将登录成功的token值保存到本地
+                localStorage.setItem('token', res.token);
+                // 跳转的主页
+                location.href = '/index.html'
+            });
+
         }
     })
 
-}
+})
